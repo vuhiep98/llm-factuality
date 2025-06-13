d5defdc0d529fa9ff7fad9115d18c71e1c8e1d11
hiepnkv <nkv.hiep@gmail.com>
2025-06-13 14:37:09 +0900

    fix bug: load Atomic Fact Generator when enable use_atomic_facts

--- modules/FActScore/factscore/factscorer.py
+++ modules/FActScore/factscore/factscorer.py
@@ -131,29 +131,29 @@ class FactScorer(object):
             assert type(topics)==type(generations)==list, "`topics` and `generations` should be lists."
             assert len(topics)==len(generations), "`topics` and `generations` should have the same length"
 
+        if self.af_generator is None:
+            # if "ChatGPT" in self.model_name:
+            self.af_generator = AtomicFactGenerator(
+                key_path=self.openai_key,
+                demon_dir=os.path.join(self.data_dir, "demos"),
+                cache_file=os.path.join(self.cache_dir, "AT_InstructGPT.pkl")
+            )
+            # elif "llama2" in self.model_name:
+            #     self.af_generator = AtomicFactGenerator(
+            #         demon_dir=os.path.join(self.data_dir, "demos"), 
+            #         model_name="llama2",
+            #         cache_file=os.path.join(self.cache_dir, "af-inst-llama-7B.pkl")
+            #     )
+            # elif "llama3" in self.model_name:
+            #     self.af_generator = AtomicFactGenerator(
+            #         demon_dir=os.path.join(self.data_dir, "demos"), 
+            #         model_name="llama3",
+            #         cache_file=os.path.join(self.cache_dir, "af-llama3.1-8B-Instruct.pkl")
+            #     )
+        
         if atomic_facts is not None:
             assert len(topics)==len(atomic_facts), "`topics` and `atomic_facts` should have the same length"
         else:
-            if self.af_generator is None:
-                # if "ChatGPT" in self.model_name:
-                self.af_generator = AtomicFactGenerator(
-                    key_path=self.openai_key,
-                    demon_dir=os.path.join(self.data_dir, "demos"),
-                    cache_file=os.path.join(self.cache_dir, "AT_InstructGPT.pkl")
-                )
-                # elif "llama2" in self.model_name:
-                #     self.af_generator = AtomicFactGenerator(
-                #         demon_dir=os.path.join(self.data_dir, "demos"), 
-                #         model_name="llama2",
-                #         cache_file=os.path.join(self.cache_dir, "af-inst-llama-7B.pkl")
-                #     )
-                # elif "llama3" in self.model_name:
-                #     self.af_generator = AtomicFactGenerator(
-                #         demon_dir=os.path.join(self.data_dir, "demos"), 
-                #         model_name="llama3",
-                #         cache_file=os.path.join(self.cache_dir, "af-llama3.1-8B-Instruct.pkl")
-                #     )
-
             if "ChatGPT" in self.model_name:
                 # estimate the total cost of atomic fact generation
                 total_words = 0
